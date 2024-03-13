b83114ef59e980dab6f08e1e287a1543e984d9cf
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2024-03-14 01:37:22 +0200

    your commit message

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
-import HomePage from "./Page/HomePage";
-import {BrowserRouter, Routes , Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBarLogin from "./Components/Uitily/NavBarLogin";
-import Footer from './Components/Uitily/Footer'
+import Footer from './Components/Uitily/Footer';
+import HomePage from "./Page/HomePage";
 import LoginPage from "./Components/Auth/LoginPage";
 import RegisterPage from "./Components/Auth/RegisterPage";
 import AllCategoryPage from "./Components/Category/AllCategoryPage";
-import AllBrandPage from "./Page/Brand/AllBrandPage"
+import AllBrandPage from "./Page/Brand/AllBrandPage";
 import ShopProductsPage from "./Page/Products/ShopProductsPage";
-
 
 function App() {
   return (
     <div className="font">
-       <NavBarLogin />
-     <BrowserRouter>
-     <Routes>
-     <Route index element={<HomePage />}  />
-     <Route path="/login" element={<LoginPage />} />
-     <Route path="/register" element={<RegisterPage />} />
-     <Route path="/allcategory" element={<AllCategoryPage />} />
-     <Route path="/allbrand" element={<AllBrandPage />} />
-     <Route path="/products" element={<ShopProductsPage />} />
-     </Routes>
-     </BrowserRouter>
-     <Footer /> 
+      <NavBarLogin />
+      <Router basename="/Electronic-shop">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="allcategory" element={<AllCategoryPage />} />
+          <Route path="allbrand" element={<AllBrandPage />} />
+          <Route path="products" element={<ShopProductsPage />} />
+        </Routes>
+      </Router>
+      <Footer /> 
     </div>
   )
 }
 
 export default App;
+
